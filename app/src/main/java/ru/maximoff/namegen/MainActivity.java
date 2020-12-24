8fae405b108bab5ce6dfcc7b3af6ea4c23cc782a
Maximoff <maksimoff.andrew.7@gmail.com>
2020-12-24 14:40:40 +0300

    update

--- app/src/main/java/ru/maximoff/namegen/MainActivity.java
+++ app/src/main/java/ru/maximoff/namegen/MainActivity.java
@@ -16,28 +16,50 @@ import ru.maximoff.namegen.R;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.AdapterView;
 import android.widget.Adapter;
+import android.widget.CheckBox;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		final String[] values = new String[11];
-		for (int i = 0; i <= 10; i++) {
-			values[i] = String.valueOf(i + 5);
+		final String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		final NameGenerator generator = new NameGenerator();
+		final String[] lettArray = new String[letters.length() + 1];
+		lettArray[0] = " ";
+		for (int i = 0; i < letters.length(); i++) {
+			lettArray[i + 1] = String.valueOf(letters.charAt(i));
+		}
+		ArrayAdapter<String> lettAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, lettArray);
+		final Spinner firstLett = findViewById(R.id.mainSpinner2);
+		firstLett.setAdapter(lettAdapter);
+		int size = generator.max() - generator.min();
+		final String[] values = new String[size + 1];
+		for (int i = 0; i <= size; i++) {
+			values[i] = String.valueOf(i + generator.min());
 		}
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, values);
 		final Spinner range = findViewById(R.id.mainSpinner1);
 		range.setAdapter(adapter);
-		range.setSelection(2);
+		range.setSelection(generator.def() - generator.min());
+		final CheckBox firstCap = findViewById(R.id.mainCheckBox1);
+		final CheckBox allCap = findViewById(R.id.mainCheckBox2);
 		final TextView text = findViewById(R.id.mainTextView1);
 		final Button button = findViewById(R.id.mainButton1);
-		final NameGenerator generator = new NameGenerator();
-		generator.setLength(7);
-		range.setOnItemSelectedListener(new OnItemSelectedListener() {
+		OnClickListener listener = new OnClickListener() {
+			@Override
+			public void onClick(View p1) {
+				generator.setFirstChar(lettArray[firstLett.getSelectedItemPosition()]);
+				generator.firstToUpper(firstCap.isChecked());
+				generator.allToUpper(allCap.isChecked());
+				generator.setLength(range.getSelectedItemPosition() + generator.min());
+				text.setText(generator.getName());
+			}
+		};
+		firstLett.setOnItemSelectedListener(new OnItemSelectedListener() {
 				@Override
 				public void onItemSelected(AdapterView<?> p1, View p2, int p3, long p4) {
-					generator.setLength(p3 + 5);
+					generator.setFirstChar(lettArray[p3]);
 					text.setText(generator.getName());
 				}
 
@@ -45,13 +67,20 @@ public class MainActivity extends Activity {
 				public void onNothingSelected(AdapterView<?> p1) {
 				}
 			});
-		button.setOnClickListener(new OnClickListener() {
+		range.setOnItemSelectedListener(new OnItemSelectedListener() {
 				@Override
-				public void onClick(View p1) {
-					generator.setLength(range.getSelectedItemPosition() + 5);
+				public void onItemSelected(AdapterView<?> p1, View p2, int p3, long p4) {
+					generator.setLength(p3 + generator.min());
 					text.setText(generator.getName());
 				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> p1) {
+				}
 			});
+		button.setOnClickListener(listener);
+		allCap.setOnClickListener(listener);
+		firstCap.setOnClickListener(listener);
 		text.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View p1) {
@@ -66,5 +95,11 @@ public class MainActivity extends Activity {
 		ClipData clip = ClipData.newPlainText("Copied Text", text);
 		clipboard.setPrimaryClip(clip);
 		Toast.makeText(this, getString(R.string.copied, text), Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		finish();
 	}
 }
--- app/src/main/java/ru/maximoff/namegen/NameGenerator.java
+++ app/src/main/java/ru/maximoff/namegen/NameGenerator.java
@@ -1,14 +1,21 @@
 package ru.maximoff.namegen;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class NameGenerator {
+	private final int MIN = 3; // минимальная длина
+	private final int MAX = 15; // максимальная длина
+	private final int DEF = 7; // длина по умолчанию
+	
 	private final int diff = 26;
 	private final int startChar = 97;
 	private String lastName = "";
-	private int length = 7;
+	private int length = DEF;
+	private char firstChar = ' ';
+	private boolean firstToUpper = true;
+	private boolean allToUpper = false;
 	private List<Character> vowels = new ArrayList<>();
 
 	public NameGenerator() {
@@ -23,45 +30,86 @@ public class NameGenerator {
 	public String getLast() {
 		return lastName;
 	}
+	
+	public int min() {
+		return MIN;
+	}
+	
+	public int max() {
+		return MAX;
+	}
+	
+	public int def() {
+		return DEF;
+	}
 
 	public void setLength(int length) {
-		if (length < 5 || length > 15) {
-			length = 7;
+		if (length < MIN || length > MAX) {
+			length = DEF;
 		}
 		this.length = length;
 	}
 
+	public void firstToUpper(boolean value) {
+		this.firstToUpper = value;
+	}
+
+	public void allToUpper(boolean value) {
+		this.allToUpper = value;
+	}
+
+	public void setFirstChar(String lett) {
+		this.firstChar = Character.toLowerCase(lett.charAt(0));
+	}
+
 	public String getName() {
-		for (;;) {
-			Random random = new Random(System.currentTimeMillis());
+		while (true) {
+			Random random = new Random();
 			char[] charArray = new char[length];
-			for (int i = 0; i < length; i++) {
-				if (positionIsOdd(i)) {
-					charArray[i] = getVowel(random);
-				} else {
-					charArray[i] = getConsonant(random);
+			int i = 0, x = random.nextInt(2);
+			if (firstChar != ' ') {
+				charArray[0] = firstChar;
+				i++;
+				x = 0;
+				if (vowels.contains(firstChar)) {
+					x++;
 				}
 			}
-			charArray[0] = Character.toUpperCase(charArray[0]);
+			for (; i < length; i++,x++) {
+				if (checkPosition(x)) {
+					charArray[i] = getVowel(random);
+				} else {
+					charArray[i] = getRandChar(random);
+				}
+			}
+			if (firstToUpper) {
+				charArray[0] = Character.toUpperCase(charArray[0]);
+			}
 			String generatedName = new String(charArray);
+			if (allToUpper) {
+				generatedName = generatedName.toUpperCase();
+			}
 			if (!generatedName.equals(lastName)) {
 				lastName = generatedName;
 				return generatedName;
+			} else {
+				return getName();
 			}
 		}
 	}
 
-	private boolean positionIsOdd(int i) {
+	private boolean checkPosition(int i) {
 		return i % 2 == 0;
 	}
 
-	private char getConsonant(Random random) {
-		for (;;) {
+	private char getRandChar(Random random) {
+		while (true) {
 			char currentChar = (char) (random.nextInt(diff) + startChar);
-			if (vowels.contains(currentChar))
+			if (vowels.contains(currentChar)) {
 				continue;
-			else
+			} else {
 				return currentChar;
+			}
 		}
 	}
 
